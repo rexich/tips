7e1a065b7a6428ff59020368afae98693e6630c1
Filip Dimovski <rexich@riseup.net>
2017-01-23 14:44:34 +0100

    Wrote the Kitchen 101; created recipes, wrote Serverspec and BATS tests; verified and tested everything using Kitchen and Vagrant, all is well.

---
+++ kitchen-git-cookbook/test/integration/server/serverspec/git_daemon_spec.rb
@@ -0,0 +1,16 @@
+require "serverspec"
+
+# Required by Serverspec
+set :backend, :exec
+
+describe "Git Daemon" do
+
+  it "is listening on port 9418" do
+    expect(port(9418)).to be_listening
+  end
+
+  it "has a running service of git-daemon" do
+    expect(service("git-daemon")).to be_running
+  end
+
+end
